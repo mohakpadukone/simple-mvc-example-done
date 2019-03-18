b71f8c2af6ddeb78e5910472845967090e52d0b1
mohakpadukone <mvp2395@rit.edu>
2019-03-17 21:58:32 -0400

    Add files via upload

--- server/router.js
+++ server/router.js
@@ -16,9 +16,10 @@ const router = (app) => {
   app.get('/page1', controllers.page1);
   app.get('/page2', controllers.page2);
   app.get('/page3', controllers.page3);
+  app.get('/page4', controllers.page4);
   app.get('/getName', controllers.getName);
   app.get('/findByName', controllers.searchName);
-
+  app.get('/searchDogName', controllers.searchDogName);
     // whenever someone goes to the site without a path (AKA the home page), call controllers.index
     // For example www.webpage.com
   app.get('/', controllers.index);
@@ -32,6 +33,8 @@ const router = (app) => {
 
     // When someone POSTS to /updateLast, call controllers.updateLast
   app.post('/updateLast', controllers.updateLast);
+
+  app.post('/setDogName', controllers.setDogName);
 };
 
 // export the router function
